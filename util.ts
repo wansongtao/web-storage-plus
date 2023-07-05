085e9f946e1e36c8ce798478c1bea75cd12fa145
wansongtao <18101837209@163.com>
2023-07-05 17:53:41 +0800

    todo: 初步完成stringify与parse方法的增强

--- util.ts
+++ util.ts
@@ -1,20 +1,23 @@
 /**
- * 将js对象或值转换为字符串
+ * enhanced JSON.stringify, support function, regexp, date, undefined, 
+ * NaN, Infinity, -Infinity, bigint
  * @param obj
+ * @example new Date() => "'type': {{date}}-'value':{{1625673600000}}"
  * @returns
  */
-const stringify = (obj: any): string => {
-  return JSON.stringify(obj, (key, value) => {
+export const stringify = (obj: any): string => {
+  Date.prototype.toJSON = function () {
+    return `'type': {{date}}-'value':{{${this.getTime()}}}`;
+  };
+
+  return JSON.stringify(obj, (_key, value) => {
     if (value instanceof Function) {
       return `'type': {{function}}-'value':{{${value
         .toString()
         .replace(/^function/, '')}}}`;
     }
     if (value instanceof RegExp) {
-      return `'type': {{regexp}}-'value':{{${value.toString().slice(1, -1)}}}`;
-    }
-    if (obj[key] instanceof Date) {
-      return `'type': {{date}}-'value':{{${obj[key].getTime()}}}`;
+      return `'type': {{regexp}}-'value':{{${value.toString()}}}`;
     }
     if (value === undefined) {
       return `'type': {{undefined}}-'value':{{undefined}}`;
@@ -28,23 +31,75 @@ const stringify = (obj: any): string => {
     if (value === -Infinity) {
       return `'type': {{number}}-'value':{{-Infinity}}`;
     }
+    if (typeof value === 'bigint') {
+      return `'type': {{bigint}}-'value':{{${value.toString()}}}`;
+    }
 
     return value;
   });
 };
 
-const a = {
-  name: 'wansongtao',
-  age: 25,
-  hi() {
-    return 'my name is wansongtao';
-  },
-  reg: /[0-9]+/,
-  e: undefined,
-  a: Infinity,
-  b: -Infinity,
-  nan: NaN,
-  date: new Date()
+/**
+ * enhanced JSON.parse, support function, regexp, date, undefined, 
+ * NaN, Infinity, -Infinity, bigint
+ * @param str 
+ * @example "'type': {{regexp}}-'value':{{/0/gi}}" => new RegExp('0', 'gi')
+ * @returns 
+ */
+export const parse = <T = any>(str: string): T => {
+  const regexp =
+    /^'type': {{(number|undefined|date|regexp|function|bigint)}}-'value':{{([^]+)}}$/;
+
+  type IType = 'number' | 'undefined' | 'date' | 'regexp' | 'function' | 'bigint';
+  const strategies: Record<IType, (text: string) => any> = {
+    function: (text: string) => {
+      if (/^\(\) =>/.test(text)) {
+        return new Function(`return ${text}`)();
+      }
+
+      return new Function(`return function ${text}`)();
+    },
+    regexp: (text: string) => {
+      const regMatch = /^\/([^]+)\/([gimsuy]{0,})$/.exec(text);
+      if (regMatch) {
+        return new RegExp(regMatch[1], regMatch[2]);
+      }
+
+      return new RegExp(text);
+    },
+    date: (text: string) => {
+      return new Date(Number(text));
+    },
+    number: (text: string) => {
+      return Number(text);
+    },
+    undefined: (_text?: string) => {
+      return undefined;
+    },
+    bigint: (text: string) => {
+      return BigInt(text);
+    }
+  };
+
+  return JSON.parse(str, (_key, value) => {
+    if (typeof value !== 'string') {
+      return value;
+    }
+
+    try {
+      const match = regexp.exec(value);
+      if (!match) {
+        return value;
+      }
+
+      const type = match[1] as IType;
+      if (strategies[type]) {
+        return strategies[type](match[2]);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    return value;
+  });
 };
-const test = stringify(a);
-console.log(test);
