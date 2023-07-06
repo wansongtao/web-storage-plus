b38c30805a85d404289c4a76a664c91ce4c1e59c
wansongtao <18101837209@163.com>
2023-07-06 15:42:47 +0800

    refactor: 文件结构重构，支持设置js转换json函数，json转换js函数

---
+++ src/storage.ts
@@ -0,0 +1,172 @@
+let globalPrefix = 'st-';
+/**
+ * @param {string} prefix
+ */
+export const setGlobalPrefix = (prefix: string) => {
+  globalPrefix = prefix;
+};
+
+type IStringifyFn = (data: any) => string;
+let globalStringifyFn: IStringifyFn | undefined = undefined;
+/**
+ * set a global method to converts a JavaScript value to a JSON string
+ * @param {function} fn
+ */
+export const setGlobalStringifyFn = (fn: IStringifyFn) => {
+  globalStringifyFn = fn;
+};
+
+export interface IConfig {
+  isLocalStorage?: boolean;
+  maxAge?: number;
+  prefix?: string;
+  stringifyFn?: IStringifyFn;
+}
+
+interface IStorage<T = unknown> {
+  data: T;
+  expire: number;
+}
+
+/**
+ * set localStorage/sessionStorage, if maxAge is set, it will be expired
+ * after maxAge seconds
+ * @param {string} key
+ * @param {any} value
+ * @param {object} [config]
+ * @param {boolean} [config.isLocalStorage] default true,
+ * true - localStorage, false - sessionStorage
+ * @param {number} [config.maxAge] Expiration time, in seconds
+ * @param {string} [config.prefix] default globalPrefix
+ * @param {function} [config.stringifyFn] default globalStringifyFn,
+ * undefined - JSON.stringify,
+ * This method converts the JavaScript value to a JSON string
+ * @returns {void}
+ */
+export const setStorage = <T = unknown>(
+  key: string,
+  value: T,
+  {
+    isLocalStorage = true,
+    maxAge,
+    prefix = globalPrefix,
+    stringifyFn = globalStringifyFn
+  }: IConfig = {}
+): void => {
+  try {
+    const storage: IStorage = { data: value, expire: 0 };
+
+    if (maxAge) {
+      storage.expire = Date.now() + maxAge * 1000;
+    }
+    if (!stringifyFn) {
+      stringifyFn = JSON.stringify;
+    }
+
+    const name = `${prefix}${key}`;
+    const json = stringifyFn(storage);
+
+    if (isLocalStorage) {
+      localStorage.setItem(name, json);
+      return;
+    }
+    sessionStorage.setItem(name, json);
+  } catch (ex) {
+    console.error(ex);
+  }
+};
+
+export interface IGetStorageConfig {
+  isLocalStorage?: boolean;
+  prefix?: string;
+  isDeleteExpired?: boolean;
+  parseFn?: IParseFn;
+}
+
+type IParseFn = (data: string) => any;
+let globalParseFn: IParseFn | undefined = undefined;
+/**
+ * set a global method that parses a JSON string
+ * @param {function} fn
+ */
+export const setGlobalParseFn = (fn: IParseFn) => {
+  globalParseFn = fn;
+};
+
+/**
+ * @param {string} key
+ * @param {object} [config]
+ * @param {boolean} [config.isLocalStorage] default true,
+ * true - localStorage, false - sessionStorage
+ * @param {string} [config.prefix] default globalPrefix
+ * @param {boolean} [config.isDeleteExpired] default false, delete expired data
+ * @param {function} [config.parseFn] default globalParseFn, undefined - JSON.parse,
+ * This method parses a JSON string
+ * @returns
+ */
+export const getStorage = <T = unknown>(
+  key: string,
+  {
+    isLocalStorage = true,
+    prefix = globalPrefix,
+    isDeleteExpired,
+    parseFn = globalParseFn
+  }: IGetStorageConfig = {}
+) => {
+  try {
+    const name = `${prefix}${key}`;
+    const jsonText = isLocalStorage
+      ? localStorage.getItem(name)
+      : sessionStorage.getItem(name);
+
+    if (jsonText === null) {
+      console.warn(`not found ${name}`);
+      return null;
+    }
+    if (!parseFn) {
+      parseFn = JSON.parse;
+    }
+
+    const storage: IStorage<T> = parseFn(jsonText);
+    if (storage.expire && storage.expire <= Date.now()) {
+      if (isDeleteExpired) {
+        removeStorage(key, { isLocalStorage, prefix });
+      }
+
+      console.warn(`${name}: data expired!`);
+      return null;
+    }
+
+    return storage.data;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+/**
+ * @param {string} key
+ * @param {object} [config]
+ * @param {boolean} [config.isLocalStorage] true - localStorage, false - sessionStorage, default true
+ * @param {string} [config.prefix] default globalPrefix
+ */
+export const removeStorage = (
+  key: string,
+  {
+    isLocalStorage = true,
+    prefix = globalPrefix
+  }: { isLocalStorage?: boolean; prefix?: string } = {}
+) => {
+  const name = `${prefix}${key}`;
+
+  try {
+    if (isLocalStorage) {
+      localStorage.removeItem(name);
+      return;
+    }
+
+    sessionStorage.removeItem(name);
+  } catch (ex) {
+    console.error(ex);
+  }
+};
---
+++ src/util.ts
@@ -0,0 +1,131 @@
+/**
+ * enhanced JSON.stringify, support function, regexp, date, undefined,
+ * NaN, Infinity, -Infinity, bigint
+ * @param obj
+ * @example new Date(1625673600000) => "type: {{date}}-value: {{1625673600000}}"
+ * @example "type: {{test}}-value: {{t}}" => "type: {{original}}-value: {{type: {{test}}-value: {{t}}}}"
+ * @returns
+ */
+export const stringify = (obj: any): string => {
+  const toJSON = Date.prototype.toJSON;
+  Date.prototype.toJSON = null as any;
+
+  const regexp =
+    /^type: {{(original|number|undefined|date|regexp|function|bigint)}}-value: {{([^]+)}}$/;
+
+  const jsonStr = JSON.stringify(obj, (_key, value) => {
+    if (value instanceof Function) {
+      return `type: {{function}}-value: {{${value
+        .toString()
+        .replace(/^function/, '')}}}`;
+    }
+    if (value instanceof RegExp) {
+      return `type: {{regexp}}-value: {{${value.toString()}}}`;
+    }
+    if (value === undefined) {
+      return `type: {{undefined}}-value: {{undefined}}`;
+    }
+    if (Number.isNaN(value)) {
+      return `type: {{number}}-value: {{NaN}}`;
+    }
+    if (value === Infinity) {
+      return `type: {{number}}-value: {{Infinity}}`;
+    }
+    if (value === -Infinity) {
+      return `type: {{number}}-value: {{-Infinity}}`;
+    }
+    if (typeof value === 'bigint') {
+      return `type: {{bigint}}-value: {{${value.toString()}}}`;
+    }
+    if (value instanceof Date) {
+      return `type: {{date}}-value: {{${value.getTime()}}}`;
+    }
+    if (regexp.test(value)) {
+      return `type: {{original}}-value: {{${value}}}`;
+    }
+
+    return value;
+  });
+
+  Date.prototype.toJSON = toJSON;
+  return jsonStr;
+};
+
+/**
+ * enhanced JSON.parse, support function, regexp, date, undefined,
+ * NaN, Infinity, -Infinity, bigint
+ * @param str
+ * @example "type: {{regexp}}-value: {{/0/gi}}" => new RegExp('0', 'gi')
+ * @returns
+ */
+export const parse = <T = any>(str: string): T => {
+  const regexp =
+    /^type: {{(original|number|undefined|date|regexp|function|bigint)}}-value: {{([^]+)}}$/;
+
+  type IType =
+    | 'original'
+    | 'number'
+    | 'undefined'
+    | 'date'
+    | 'regexp'
+    | 'function'
+    | 'bigint';
+  const strategies: Record<IType, (text: string) => any> = {
+    function: (text: string) => {
+      if (/^\(\) =>/.test(text)) {
+        return new Function(`return ${text}`)();
+      }
+
+      return new Function(`return function ${text}`)();
+    },
+    regexp: (text: string) => {
+      const regMatch = /^\/([^]+)\/([gimsuy]{0,})$/.exec(text);
+      if (regMatch) {
+        return new RegExp(regMatch[1], regMatch[2]);
+      }
+
+      throw new Error('invalid regexp');
+    },
+    date: (text: string) => {
+      return new Date(Number(text));
+    },
+    number: (text: string) => {
+      return Number(text);
+    },
+    undefined: (text: string) => {
+      if (text === 'undefined') {
+        return undefined;
+      }
+
+      throw new Error('invalid undefined');
+    },
+    bigint: (text: string) => {
+      return BigInt(text);
+    },
+    original: (text: string) => {
+      return text;
+    }
+  };
+
+  return JSON.parse(str, (_key, value) => {
+    if (typeof value !== 'string') {
+      return value;
+    }
+
+    try {
+      const match = regexp.exec(value);
+      if (!match) {
+        return value;
+      }
+
+      const type = match[1] as IType;
+      if (strategies[type]) {
+        return strategies[type](match[2]);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    return value;
+  });
+};
